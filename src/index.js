573006efc54f0dbd66a136737e05ad9119bd3b54
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2024-05-26 13:27:08 +0200

    rename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/test-point">
+        <BrowserRouter basename="/camper-quest">
           <App />
         </BrowserRouter>
       </PersistGate>
